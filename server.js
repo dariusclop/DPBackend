9484cb9911d6e5b428bfabd28664b69af7b09ca4
unknown <clop_darius@yahoo.com>
2020-04-01 22:38:02 +0300

    Add storage

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const http = require("http");
 const express = require("express");
 const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname)
+  }
+});
+const upload = multer({storage: storage });
 const bodyParser = require('body-parser');
 const firebaseService = require("./services/firebase");
 
