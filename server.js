b8c50a8f5af1be0900dcffda6b702c706336b5bc
unknown <clop_darius@yahoo.com>
2020-04-01 21:29:37 +0300

    Add parser

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const http = require("http");
 const express = require("express");
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const bodyParser = require('body-parser');
 const firebaseService = require("./services/firebase");
 
 const app = express();
 const cors = require("cors");
 
 app.use(cors());
+app.use(bodyParser());
+
+const singleUpload = upload.single('newImage');
 
 app.get('/', function(req, res) {
   res.send('on localhost server');
 })
 
-app.post('/image/upload', function(req, res) {
-  // firebaseService.uploadFile();
-  res.send('image upload');
+app.post('/image/upload',  function(req, res) {
+  singleUpload(req, res, function(err) {
+
+    if (err) {
+        return res.status(422).send({errors: [{title: 'File Upload Error', detail: err.message}] });
+    }else{
+        imageName =  req.file.filename;
+        console.log(req.file.path);
+        var imagePath = req.file.path;
+        return res.send({success:true,  imageName});
+    }
+})
 })
 
 app.post('/imageId/insert', function(req, res) {
